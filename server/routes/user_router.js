0c575970c35fe87ab5ae6c9206e45d198252f44e
EngineeringMilo <milo.regnard@telenet.be>
2025-11-19 17:03:04 +0100

    New model for relation between events and users, event pages look prettier and flow is more logical

--- server/routes/user_router.js
+++ server/routes/user_router.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const Event = require('../models/Event');
 const User = require('../models/User');
+const Registration = require('../models/Registration');
 const bcrypt = require('bcrypt');//to hash passwords
 const jwt = require('jsonwebtoken')
 const cookieParser = require('cookie-parser');
@@ -25,6 +26,24 @@ const authMiddleware = async (req,res,next) => {
     } catch (error) {
         return res.render('fourOone');   
     }
+}
+
+// This is an extra check just to see if the user is logged in and if so to add a 'join' button to the event page
+const eventAuthMiddleware = async (req,res,next) => {
+  const token = req.cookies.token;
+  if (!token) {
+    req.user = null;
+    return next();
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await User.findById(decoded.userID);
+    user = req.user;
+  } catch (err) {
+    req.user = null; // guest mode
+  }
+  next();
 }
 
 function parseYouTubeUrl(url) {
@@ -84,8 +103,10 @@ router.get('/profile', authMiddleware, async (req, res) => {
             return res.render('adminpage', {user: req.user, users, toBeApprovedUsers})
         }
         const events = await Event.find({createdBy: req.user._id});//fetch all events created by logged in user
+        const regs = await Registration.find({ userId: req.user._id }).populate("eventId");
+        console.log(regs)
         await req.user.populate('joinedEvents');
-        res.render('profile', {user: req.user, events, joinedEvents: req.user.joinedEvents});
+        res.render('profile', {user: req.user, events, joinedEvents: req.user.joinedEvents, regs});
     } catch (error) {
         console.log(error);
     }
@@ -167,6 +188,35 @@ router.post('/createevent', authMiddleware, async (req, res) => {
         console.log(error);
     }
 })
+
+router.post('/event/:id/join', eventAuthMiddleware, async (req, res) => {
+  if (!req.user) {
+    return res.redirect('/login');
+  }
+
+  try {
+    const event = await Event.findById(req.params.id);
+    if (!event) return res.status(404).send('Event not found');
+    const existingRegistration = await Registration.findOne({
+        userId: req.user.id,
+        eventId: req.params.id,
+    });
+    const user = req.user;
+    if (existingRegistration){
+        await Registration.findByIdAndDelete(existingRegistration.id)
+    }
+    else if(existingRegistration === null){
+        await Registration.create({
+            userId: req.user.id,
+            eventId: req.params.id,
+        });
+    }
+    res.redirect(`/event/${event._id}`);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Something went wrong.');
+  }
+});
 
 
 /**
